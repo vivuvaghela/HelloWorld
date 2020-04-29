cffa847c8ca4db27ffb86008bf6d4904a0ee665a
vivuvaghela <64396819+vivuvaghela@users.noreply.github.com>
2020-04-29 19:55:56 +0530

    Only make it "Hello, World" program

--- Hello.java
+++ Hello.java
@@ -1,11 +1,6 @@
-public class Hello{
-         
-              public static void main(String[] args){
-
-                   for(int i=1;i<=5;i++){
-                       
-                           System.out.println("Hello World ... "+i);
-                   }
-               }
-
+public class HelloWorld {
+   public static void main(String[] args) {
+      // prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
 }
