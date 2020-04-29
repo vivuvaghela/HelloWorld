229b3758fd6e748c7f2fed656dfed6cc4599ab43
vivuvaghela <64396819+vivuvaghela@users.noreply.github.com>
2020-04-29 20:25:17 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class Hello{
    public static void main(String[] args) {
       // prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
