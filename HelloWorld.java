a6fd5396c4e77aa3cfdfffa97eb3ca5c94db0fd2
vivuvaghela <64396819+vivuvaghela@users.noreply.github.com>
2020-05-06 11:44:26 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
-      // prints "Om Shanti, mera bab" in the terminal window.
-      System.out.println("Om Shanti, mera bab");
+      // prints "Om namahshivay" in the terminal window.
+      System.out.println("Om namahshivay");
    }
 }
